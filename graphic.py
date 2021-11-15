3f20b9c7b86ca40f1b18082939e9d39cfb4337a1
Terezka :3 <tereza.jatiova@seznam.cz>
2021-11-15 22:29:35 +0100

    added new constant

--- graphic.py
+++ graphic.py
@@ -2,7 +2,8 @@ import pygame
 from pygame.locals import *
 
 WINDOW_WIDTH = 420
-WINDOW_HEIGHT = 420
+WINDOW_HEIGHT = 600 
+PLAY_FIELD_HEIGHT = 420 
 BASIC_FONT = None
 BASIC_FONT_SIZE = 50
 CELL_SIZE = 10
@@ -16,6 +17,6 @@ BG_COLOR = (240, 240, 240)
 def draw_game():
     DISPLAY_SURFACE.fill(BG_COLOR)
     for x in range(0, WINDOW_WIDTH, CELL_SIZE): 
-        pygame.draw.line(DISPLAY_SURFACE, GRID_COLOR, (x, 0), (x, WINDOW_HEIGHT))
-    for y in range(0, WINDOW_HEIGHT, CELL_SIZE): 
+        pygame.draw.line(DISPLAY_SURFACE, GRID_COLOR, (x, WINDOW_HEIGHT), (x, WINDOW_HEIGHT - PLAY_FIELD_HEIGHT))
+    for y in range(WINDOW_HEIGHT - PLAY_FIELD_HEIGHT, WINDOW_HEIGHT, CELL_SIZE): 
         pygame.draw.line(DISPLAY_SURFACE, GRID_COLOR, (0, y), (WINDOW_WIDTH, y))
