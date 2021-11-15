aafeb5b0a826a7ebbdd9352c4df2654efc726ea0
Terezka :3 <tereza.jatiova@seznam.cz>
2021-11-15 18:56:29 +0100

    sad

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 DISPLAY_WIDTH = WINDOW_WIDTH #display width/height bude asi to pole s minama
 DISPLAY_HEIGHT = WINDOW_HEIGHT - 100 #potom změnit až tam budou ty stats
-CELL_SIZE: 10
+CELL_SIZE = 10
 assert DISPLAY_WIDTH % CELL_SIZE == 0, 'Window width must be a multiple of cell size.'
 assert DISPLAY_HEIGHT % CELL_SIZE == 0, 'Window height must be a multiple of cell size.'
 
 BG_COLOR = (215, 215, 215)
 GRID_COLOR = (181, 181, 181)
-BASIC_FONT = pygame.font.Font('freesansbold.ttf', 8)
 
 def main():
     global FPS_CLOCK, DISPLAY_SURFACE, BUTTONS
