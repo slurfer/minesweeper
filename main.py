09bba481aed8ba8dff78de00466fba57ad2447ac
Adéla Novotná <jormun.gandr@712.cz>
2021-11-15 18:34:45 +0100

    width height

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from pygame.locals import *
 import board
 import time
 
+WINDOW_WIDTH = 420
+WINDOW_HEIGHT = 420
+
 def main():
     global FPS_CLOCK, DISPLAY_SURFACE, BASIC_FONT, BUTTONS
 
     pygame.init()
     FPS_CLOCK = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Minesweeper")
+    while True:
+        pygame.display.update
