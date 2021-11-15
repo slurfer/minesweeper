a9d927b2c52b49dcd1022ace08cabb0a12dbde0a
Adéla Novotná <jormun.gandr@712.cz>
2021-11-15 18:59:14 +0100

    black screen works yeey

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from pygame.locals import *
-import board
+from board import *
 import time
 
 WINDOW_WIDTH = 420
 WINDOW_HEIGHT = 420
+BASIC_FONT = None
+BASIC_FONT_SIZE = 50
 
 def main():
     global FPS_CLOCK, DISPLAY_SURFACE, BASIC_FONT, BUTTONS
@@ -12,5 +14,15 @@ def main():
     pygame.init()
     FPS_CLOCK = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Minesweeper")
-    while True:
-        pygame.display.update
+    BASIC_FONT = pygame.font.Font('freesansbold.ttf', BASIC_FONT_SIZE)
+    board = Board()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                running = False
+            pygame.display.flip()
+        pygame.quit
+
+if __name__ == "__main__":
+    main()
